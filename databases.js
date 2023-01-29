ab7004ba3903e9e5760f93abde46f6ba99127867
Adam-Marx <adamalexmarx@gmail.com>
2023-01-28 19:38:29 -0800

    Add seperate file for databases

---
+++ databases.js
@@ -0,0 +1,25 @@
+const urlDatabase = {
+    b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW",
+    },
+    i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW",
+    },
+  };
+  
+  const users = {
+    userRandomID: {
+      id: "userRandomID",
+      email: "one@mail.com",
+      password: "pw",
+    },
+    user2RandomID: {
+      id: "user2RandomID",
+      email: "two@mail2.com",
+      password: "pw2",
+    },
+  };
+
+  module.exports = { urlDatabase, users};
